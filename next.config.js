67909939296d834982591bf1f808f40495bd8c32
Hasan Abdirahman <hasanabdirahman.ao@gmail.com>
2024-04-11 09:08:04 -0400

    added some files to the nextConfig

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    outputFileTracingIncludes: {
+      "/": ["./meals.db"],
+    },
+  },
   images: {
     remotePatterns: [
       {
